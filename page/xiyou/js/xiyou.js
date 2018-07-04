e72fac2888d3a0713d2ebb3e6334f2635d65dd7c
djchen <djchen@iflytek.com>
2018-07-04 22:06:08 +0800

    [代码新增]我的喜友圈

---
+++ page/xiyou/js/xiyou.js
@@ -0,0 +1,14 @@
+var ss = '<div class="c"> <p class="hottitle">来啊，这里是标题这里是标题，来啊，这里是标题这里是标题</p> <div class="a"> <img class="hotimg" src="./img/test.jpg" height="405" width="640"/> <div class="b"> <p class="bp">看到组员@金盆洗手的帖子，特来参加一波。家里还有存货，稍后回到家整理贴上来，换算成人民币计算在内。 时间：2018/5/3—2018/6/2备注：存货只计算花了钱的。亲人赠送、调料油烟酱醋等除...</p> <div class="bf"><p>来自<a>jackwolf</a>圈子</p>&nbsp;&nbsp;&nbsp; <p>2018-06-28</p></div> </div> </div> <hr/> </div>'
+
+$(function () {
+    $("#arrcontent").append(ss);
+    $("#arrcontent").append(ss);
+    $("#arrcontent").append(ss);
+    $("#arrcontent").append(ss);
+    $("#arrcontent").append(ss);
+    $("#arrcontent").append(ss);
+    $("#arrcontent").append(ss);
+    $("#arrcontent").append(ss);
+    $("#arrcontent").append(ss);
+})
+

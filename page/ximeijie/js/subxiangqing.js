fe7934bc20d0491f7434379d819a1530d9a842f6
bjhan <2452317299@qq.com>
2018-07-25 22:49:20 +0800

    dd

---
+++ page/ximeijie/js/subxiangqing.js
@@ -0,0 +1,25 @@
+$(function(){
+
+
+    $('.range-slider').jRange({
+
+        from: 2000,
+
+        to: 800000,
+
+        step: 1,
+
+        scale: [2000,800000],
+
+        format: '%s',
+
+        width: 120,
+
+        showLabels: true,
+
+        isRange : true
+
+    });
+
+
+});

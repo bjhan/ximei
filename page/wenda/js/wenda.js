4dcb55e8fe462b08d0b3787cc0c6c05ded7f3dca
jiayi0730 <905823512@qq.com>
2018-08-10 20:34:54 +0800

    问答

--- page/wenda/js/wenda.js
+++ page/wenda/js/wenda.js
@@ -1,4 +1,17 @@
 $(function () {
+    showHotQuestion();
+
+    // 行数限制插件
+    clamp();
+    function clamp() {
+        var titles = document.getElementsByClassName("p-title");
+        var answers = document.getElementsByClassName("p-answer");
+        for (var i = 0; i < titles.length; i++){
+            $clamp(titles[i], {clamp: 3});
+            $clamp(answers[i], {clamp: 3});
+        }
+    }
+
     // 滚动监听
     var valTop = $(".content-top").offset().top;
     $(window).scroll(function(){
@@ -6,6 +19,9 @@ $(function () {
         if (winPos >= valTop){
             var width = $(window).width()+1;
             var left = $(".content-column").offset().left;
+            if (left == 0){
+                left = $(".content-question-search").offset().left;
+            }
             $(".to-top").css("width",width);
             $(".to-top").css("position","fixed");
             $(".to-top").css("top","0");
@@ -23,7 +39,8 @@ $(function () {
             $(".to-top").css("left","0");
             $(".to-top").css("margin-top","66");
             $(".to-top").css("background","rgba(241,241,241,0.3)");
-            $(".content-top").css("margin-left","0");
+            $(".content-top-top").css("margin-left","68px");
+            $(".content-top-bottom").css("margin-left","68px");
             $(".content").css("margin-top","31px");
         }
 
@@ -44,6 +61,7 @@ $(function () {
                 }
             },
             afterBlur: function(e){
+                this.sync();
                 // console.log(editor.html());
                 if(editor.html() == '<br/>' || editor.html() == ''){
                     editor.html('<span style="color:#9B9B9B;">写回答……</span>');
@@ -59,19 +77,156 @@ $(function () {
 
     // 切换提问栏目
     $(".content-column-title").click(function () {
+        // 切换样式
         var id = this.id;
         $(".content-column-title").css("background", "");
         $(".content-column-title a").css("color", "#000");
         $(this).css("background", "#A978D6");
         $("#" + id + " a").css("color", "#fff");
+
+        // 切换内容
+        var num = $(this).attr("num");
+        // 热门问题
+        if (num == 0){
+            showHotQuestion();
+            clamp();
+        }else if (num == 1){
+        // 最新问题
+            showNewQuestion();
+            clamp();
+        }else if (num == 2){
+        // 我的提问
+            showMyQuestion();
+            clamp();
+        }else if (num == 3){
+        // 我的回答
+            showMyAnswer();
+            clamp();
+        }
+
+    });
+
+    // 展示热门问题或最新问题
+    function showQuestionKuai() {
+        var outerone = $('<div class="content-hot content-hot-first"></div>');
+        var outer = $('<div class="content-hot"></div>');
+        var inner = '<!--题目-->'+
+            '<div class="content-hot-title pointer"><a href="wendadetail.html?id=1">'+
+            '<p class="p-title pointer">'+
+            'adsfafasdf asdf dsa fsdf sdar热门问题热门问题热门问题热门问题热门问题热门问题热门问题热门问题热门问题热门问题热门问题热门问题热门问题热门问题热门问题热门问题热门问题热门问题'+
+            '热门问题热门问题热门问题热门问aaa题热门问题热门问题热门问题热门问题热门问题热门问题热门g 问题热门问题热门问题热门问题热门问题热门问题热门问题热门问题'+
+            '</p></a>'+
+            '</div>'+
+            '<!--用户信息-->'+
+            '<div class="content-hot-info pointer">'+
+            '<div class="content-hot-img"><img src="img/img_1.png"></div>'+
+            '<div class="content-hot-username">Mary McCormick</div>'+
+            '<div class="content-hot-level">Lv1</div>'+
+            '</div>'+
+            '<!--回答-->'+
+            '<div class="content-hot-answer pointer"><a href="wendadetail.html?id=1">'+
+            '<p class="p-answer pointer">'+
+            '这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，'+
+            '这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，'+
+            '这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，'+
+            '这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，'+
+            '这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，'+
+            '这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，这里是回答，'+
+            '</p></a>'+
+            '<!--点赞-->'+
+            '<div class="content-hot-praise pointer">'+
+            '<div class="content-hot-praiseimg"><img src="img/zan.png"></div>'+
+            '<div class="content-hot-praisenum">356</div>'+
+            '</div>'+
+            '<div class="content-hot-about">'+
+            '<!--回复-->'+
+            '<div class="content-hot-replyimg"><img src="img/reply.png"></div>'+
+            '<div class="content-hot-reply">共<span>xxxx</span>条回复</div>'+
+            '<!--收藏-->'+
+            '<div class="pointer collect" style="float: left;">'+
+            '<div class="content-hot-collectionimg"><img src="img/uncollection.png"></div>'+
+            '<div class="content-hot-collection">收藏</div>'+
+            '</div>'+
+            '</div>'+
+            '</div>';
+        var more = '<div class="content-bottom-more pointer">查看更多</div>';
+        outerone.append(inner);
+        outer.append(inner);
+        outer.append(more);
+        $("#content-list").append(outerone);
+        $("#content-list").append(outer);
+    }
+
+    // 展示我的提问或我的回答
+    function showQuestionTiao() {
+        var outerone = $('<div class="content-search-list content-search-listfirst"></div>');
+        var outer = $('<div class="content-search-list"></div>');
+        var inner = '<div>'+
+            '<div class="content-list-point"></div>'+
+            '<div class="content-list-title"><a href="wendadetail.html?id=1">'+
+            '<p class="p-title pointer">'+
+            '这里是热门问题的问题，这里是人们问题的问题？这里是热门问题的问题，这里是人们问题的问题？'+
+            '这里是热门问题的问题，这里是人们问题的问题？这里是热门问题的问题，这里是人们问题的问题？'+
+            '</p></a>'+
+            '</div>'+
+            '</div>'+
+            '<div class="content-list-bottom">'+
+            '<div class="content-list-focus"><span class="content-list-focusnum">15</span>个关注</div>'+
+            '<div class="content-list-answer"><span class="content-list-answernum">13</span>个回答</div>'+
+            '<div class="content-list-time">2018-06-05</div>'+
+            '</div>';
+        outerone.append(inner);
+        outer.append(inner);
+        $("#content-list").append(outerone);
+        $("#content-list").append(outer);
+    }
+
+    // 热门问题
+    function showHotQuestion() {
+        $("#content-list").empty();
+        showQuestionKuai();
+    }
+
+    // 最新问题
+    function showNewQuestion() {
+        $("#content-list").empty();
+        showQuestionKuai();
+    }
+
+    // 我的提问
+    function showMyQuestion() {
+        $("#content-list").empty();
+        if (false){
+            var content = '<div class="div-no-question">还没有提问，<span class="askquestions pointer">提问</span>第一个问题吧！</div>';
+            $("#content-list").append(content);
+        }else {
+            showQuestionTiao();
+        }
+    }
+
+    // 我的回答
+    function showMyAnswer() {
+        $("#content-list").empty();
+        if (true){
+            var content = '<div class="div-no-question">还没有回答，看看<span class="to-hot-question pointer">为你推荐的问题</span></div>';
+            $("#content-list").append(content);
+        }else {
+            showQuestionTiao();
+        }
+    }
+
+    // 点击推荐问题
+    $("body").on('click', '.to-hot-question',function(){
+        $(".content-column-title[num$='0']") .click();
     });
 
     // 点击提问
-    $(".askquestions").click(function () {
+    $("body").on('click', '.askquestions',function(){
         var left = $(".content-column").offset().left;
         $(".content-question-inner").css("display", "block");
         $(".content-question-inner").css("left", left);
         $(".content-question-outer").css("display", "block");
+        $(".content-question-title").focus();
     });
 
     // 取消提问
@@ -96,4 +251,43 @@ $(function () {
             $(this).addClass("active");
         }
     });
+
+    // 点赞
+    $("body").on('click', '.content-hot-praise',function(){
+        $(this).children(".content-hot-praisenum").text(357);
+    });
+
+    // 提交提问
+    $(".content-question-submit").click(function () {
+        // 问题题目
+        if ($(".content-question-title").val().length < 5){
+            Toast("请添加问题标题！",1000);
+            return;
+        }
+        // 问题描述
+        var text = '<span style="color: #9B9B9B;">写回答……</span>';
+        var words = $('#mul_input').val().replace(/<[^>]+>/g,"");
+        if (words.length < 5 || $('#mul_input').val() == text){
+            Toast("请添加问题描述！",1000);
+            return;
+        }
+        $(location).attr('href', 'wendadetail.html?id=1');
+
+    });
+
+    //自定义弹框
+    function Toast(msg,duration){
+        duration=isNaN(duration)?3000:duration;
+        var m = document.createElement('div');
+        m.innerHTML = msg;
+        m.style.cssText="width: 30%;min-width: 150px;opacity: 0.5;height: 100px;color: rgb(255, 255, 255);line-height: 100px;text-align: center;border-radius: 5px;position: fixed;top: 40%;left: 35%;z-index: 999999;background: rgb(0, 0, 0);font-size: 18px;";
+        document.body.appendChild(m);
+        setTimeout(function() {
+            var d = 0.5;
+            m.style.webkitTransition = '-webkit-transform ' + d + 's ease-in, opacity ' + d + 's ease-in';
+            m.style.opacity = '0';
+            setTimeout(function() { document.body.removeChild(m) }, d * 1000);
+        }, duration);
+    }
+
 })
--- page/wenda/js/wendadetail.js
+++ page/wenda/js/wendadetail.js
@@ -1,10 +1,19 @@
 $(function () {
-    if ($("#reply-num").html() == 0){
-        hideAnswerList();
-        showNoAnswer();
-    }else {
-        showAnswerList();
-        hideNoAnswer();
+    havaAnswer();
+
+    // 判断本问题是否是本人所提 加关注 添加修改
+
+    // 判断回答中是否有本人回答
+
+    // 判断是否有问答
+    function havaAnswer() {
+        if ($("#reply-num").html() == 0){
+            hideAnswerList();
+            showNoAnswer();
+        }else {
+            showAnswerList();
+            hideNoAnswer();
+        }
     }
     
     function hideAnswerList() {
@@ -19,6 +28,41 @@ $(function () {
     function showAnswerList() {
         $(".content-reply-num").css("display", "block");
         $(".content-list").css("display", "block");
+        // $(".content-list").empty();
+        var outerOne = $('<div class="content-answer content-answer-one"></div>');
+        var outer = $('<div class="content-answer"></div>');
+        var inner = '<!--回答的头部信息-->'+
+            '<div class="content-answer-top">'+
+            '<div class="content-answer-img"><img src="img/img_1.png"></div>'+
+            '<div class="content-answer-name">Mary McCormick</div>'+
+            '<div class="content-answer-level">Lv1</div>'+
+            '<div class="content-answer-praise pointer">'+
+            '<div class="content-praise-img"><img src="img/zan.png"></div>'+
+            '<div class="content-praise-num">356</div>'+
+            '</div>'+
+            '</div>'+
+            '<div class="content-answer-hr"></div>'+
+            '<!--回答内容-->'+
+            '<div class="content-answer-text">'+
+            '这里是回复So strongly and metaphysically did I conceive of my situation then, '+
+            'that while earnestly watching his motions, I seemed distinctly to perceive that my own individuality was now merged in a joint stock company of two;'+
+            'that my free will had received a mortal wound; and that another mistake or misfortune might plunge innocent me into unmerited disaster and death.'+
+            'Therefore, I saw that here was a sort of interregnum in Providence; for its even-handed equity never could have so gross an injustice. ' +
+            'And yet still further pondering—while I jerked him now and then from between the whale and ship, which would threaten to jam him—still further pondering'+
+            '</div>'+
+            '<!--回答相关信息-->'+
+            '<div class="content-answer-bottom">'+
+            '<div class="content-answer-collect pointer">'+
+            '<div class="content-collect-img"><img src="img/meishoucang.png"></div>'+
+            '<div class="content-collect-text">收藏</div>'+
+            '</div>'+
+            '<div class="content-answer-share"><span>125</span>分享</div>'+
+            '<div class="content-answer-time">发布于2018-06-05  12:30</div>'+
+            '</div>';
+        outerOne.append(inner);
+        outer.append(inner);
+        // $(".content-list").append(outerOne);
+        // $(".content-list").append(outer);
     }
 
     function hideNoAnswer() {
@@ -65,7 +109,13 @@ $(function () {
     });
 
     // 回答问题
-    $(".writte").click(function () {
+    $("body").on('click', '.writte',function(){
+        $(".content-write").css("display","block");
+        hideNoAnswer();
+    });
+
+    // 修改问题
+    $("body").on('click', '#change-answer',function(){
         $(".content-write").css("display","block");
         hideNoAnswer();
     });
@@ -73,7 +123,7 @@ $(function () {
     // 取消回答
     $("#cancel").click(function () {
         $(".content-write").css("display","none");
-        showNoAnswer();
+        havaAnswer();
     });
 
     // 收藏
@@ -88,11 +138,12 @@ $(function () {
     });
 
     //富文本编辑器简单模式初始化
-    var editor;
+    var editor,editor1;
     KindEditor.ready(function(K) {
         editor = K.create('textarea[name="content"]', {
+            id : 'mul_input',
             resizeType : 0,
-            filterMode: false,//true时过滤HTML代码，false时允许输入任何代码。
+            filterMode: true,//true时过滤HTML代码，false时允许输入任何代码。
             allowPreviewEmoticons : false,
             allowImageUpload : false,
             cssData: 'body{font-family: 微软雅黑;font-size: 14px;padding:30px;}',
@@ -102,7 +153,7 @@ $(function () {
                 }
             },
             afterBlur: function(e){
-                // console.log(editor.html());
+                this.sync();
                 if(editor.html() == '<br/>' || editor.html() == ''){
                     editor.html('<span style="color:#9B9B9B;">写回答……</span>');
                 }
@@ -110,5 +161,121 @@ $(function () {
             items : [
                 'bold', 'italic', 'insertunorderedlist', 'image', 'media', 'link', 'emoticons']
         });
+        editor1 = K.create('textarea[name="content1"]', {
+            id : 'mul_input1',
+            resizeType : 0,
+            filterMode: false,//true时过滤HTML代码，false时允许输入任何代码。
+            allowPreviewEmoticons : false,
+            allowImageUpload : false,
+            cssData: 'body{font-family: 微软雅黑;font-size: 14px;padding:30px;}',
+            afterFocus : function(){//获得焦点 删除默认文字信息
+                if(editor1.html() == '<span style="color:#9B9B9B;">写回答……</span>'){
+                    editor1.html('');
+                }
+            },
+            afterBlur: function(e){
+                this.sync();
+                if(editor1.html() == '<br/>' || editor1.html() == ''){
+                    editor1.html('<span style="color:#9B9B9B;">写回答……</span>');
+                }
+            },//失去焦点，同步信息数据
+            items : [
+                'bold', 'italic', 'insertunorderedlist', 'image', 'media', 'link', 'emoticons']
+        });
     });
+
+    // 修改提问
+    $("#change-question").click(function () {
+        var left = $(".content").offset().left;
+        $(".content-question-inner").css("display", "block");
+        $(".content-question-inner").css("left", left);
+        $(".content-question-outer").css("display", "block");
+        $(".question-title").focus();
+    });
+
+    // 取消提问
+    $(".content-question-cancel").click(function () {
+        $(".content-question-inner").css("display", "none");
+        $(".content-question-outer").css("display", "none");
+    });
+
+    // 提交提问
+    $("body").on('click', '.content-question-submit',function(){
+        // 问题题目
+        if ($(".question-title").val().length < 5){
+            Toast("请添加问题标题！",1000);
+            return;
+        }
+        // 问题描述
+        var text = '<span style="color: #9B9B9B;">写回答……</span>';
+        var words = $('#mul_input1').val().replace(/<[^>]+>/g,"");
+        if (words.length < 5 || $('#mul_input1').val() == text){
+            Toast("请添加问题描述！",1000);
+            return;
+        }
+    });
+
+    // 提交回答
+    $("body").on('click', '.content-write-submit',function(){
+        var text = '<span style="color: #9B9B9B;">写回答……</span>';
+        var words = $('#mul_input').val().replace(/<[^>]+>/g,"");
+        if (words.length < 10 || $('#mul_input').val() == text){
+            Toast("回答不得少于10个字！",1000);
+            return;
+        }else {
+            Toast("回答成功，积分+10！",1000);
+        }
+
+        var answer = '<div class="content-answer content-answer-one">'+
+            '<!--回答的头部信息-->'+
+            '<div class="content-answer-top">'+
+            '<div class="content-answer-img"><img src="img/img_1.png"></div>'+
+            '<div class="content-answer-name">Mary McCormick</div>'+
+            '<div class="content-answer-level">Lv1</div>'+
+            '<div class="content-answer-praise pointer">'+
+            '<div class="content-praise-img"><img src="img/zan.png"></div>'+
+            '<div class="content-praise-num">356</div>'+
+            '</div>'+
+            '</div>'+
+            '<div class="content-answer-hr"></div>'+
+            '<!--回答内容-->'+
+            '<div class="content-answer-text">'+
+            $('#mul_input').val()+
+            '</div>'+
+            '<!--回答相关信息-->'+
+            '<div class="content-answer-bottom">'+
+            '<div class="content-answer-collect pointer">'+
+            '<div class="content-collect-img"><img src="img/meishoucang.png"></div>'+
+            '<div class="content-collect-text">收藏</div>'+
+            '</div>'+
+            '<div class="content-answer-share"><span>125</span>分享</div>'+
+            '<div class="content-answer-time">发布于2018-06-05  12:30</div>'+
+            '</div>'+
+            '</div>';
+        $(".content-list").prepend(answer);
+        $(".content-reply-num span").text(3);
+        $("#cancel").click();
+    });
+
+    //自定义弹框
+    function Toast(msg,duration){
+        duration=isNaN(duration)?3000:duration;
+        var m = document.createElement('div');
+        m.innerHTML = msg;
+        m.style.cssText="width: 30%;min-width: 150px;opacity: 0.5;height: 100px;color: rgb(255, 255, 255);line-height: 100px;text-align: center;border-radius: 5px;position: fixed;top: 40%;left: 35%;z-index: 999999;background: rgb(0, 0, 0);font-size: 18px;";
+        document.body.appendChild(m);
+        setTimeout(function() {
+            var d = 0.5;
+            m.style.webkitTransition = '-webkit-transform ' + d + 's ease-in, opacity ' + d + 's ease-in';
+            m.style.opacity = '0';
+            setTimeout(function() { document.body.removeChild(m) }, d * 1000);
+        }, duration);
+    }
+
+    // 点赞
+    $("body").on('click', '.content-answer-praise',function(){
+        // 数字+1 刷新页面
+        $(this).children(".content-praise-num").text(357);
+    });
+
 })

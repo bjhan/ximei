450970777a7e3edf2fe7928564fb43d7578624d5
djchen <djchen@iflytek.com>
2018-09-16 11:37:36 +0800

    [代码新增]喜友圈更新

--- page/xiyou/js/apply.js
+++ page/xiyou/js/apply.js
@@ -5,4 +5,84 @@ $(function () {
 		$("#apply").show(100);
 	})
 
+	$("#dialog").dialog({
+		autoOpen: false,
+		buttons: {
+			Cancel: function () {
+				$("#dialog").dialog("close");
+			}
+		}
+	})
+
+
+	$("#dialog2").dialog({
+		autoOpen: false,
+		buttons: {
+			Cancel: function () {
+				$("#dialog2").dialog("close");
+			}
+		}
+	})
 })
+
+
+var submit = function () {
+	var obj = checkparmet();
+	if (obj) {
+		$.ajax({
+			type: "POST",
+			contentType: "application/x-www-form-urlencoded; charset=utf-8",
+			url: baseUrl + "/xyq/forum/request",
+			data: obj,
+			success: function (msg) {
+				location.href = "./circle.html?id=" + msg.id
+			},
+			error: function (data) {
+
+			}
+		});
+	}
+
+
+}
+
+
+var getUserInfo = function () {
+	$.ajax({
+		type: "GET",
+		url: baseUrl + "/common/userInfo",
+		success: function (msg) {
+			console.log(msg);
+		}
+	});
+
+}
+
+
+var checkparmet = function () {
+	var circleName = $("#circleName").val();
+	var circleintroduce = $("#circleintroduce").val();
+	var tags = [];
+	$.each($(".tag"), function (i, n) {
+		var value = $(n).val();
+		if (value) {
+			tags.push(value);
+		}
+
+	})
+
+	if (!$("#checkSumber").is(':checked')) {
+		$("#dialog2").dialog("open");
+		return null;
+	}
+
+	if (!(circleName && circleintroduce)) {
+		$("#dialog").dialog('open');
+		return null;
+	}
+	var obj = {};
+	obj.name = circleName;
+	obj.abstrat = circleintroduce;
+	obj.tags = tags.join(",");
+	return obj;
+}
--- page/xiyou/js/topic.js
+++ page/xiyou/js/topic.js
@@ -1,10 +1,118 @@
 var baseUrl = "http://47.98.119.215:80/web/api/v1/";
 var id;
+var circleId;
 $(function () {
+	getParam();
 	getmyactivecounts();
 	getreplies();
+	getTopicDetail();
+	initDialog();
+    getCircleDetail();
 
 })
+
+
+
+var clickReply = function (id) {
+	$("#replyid").val(id);
+	$("#reply-view").dialog('open');
+}
+
+var getTopicDetail = function () {
+	$.ajax({
+		type: "GET",
+		url: baseUrl + "/xyq/post/detail?id={0}".format(id),
+		success: function (msg) {
+			console.log(msg);
+			$("#headName").text(msg.author);
+			$("#topictime").text(msg.createTime.substr(0, 10))
+			$("#priaseCount").text(msg.priaseCount);
+			$("#description").html(msg.content)
+
+		}
+	});
+}
+
+
+var getCircleDetail = function () {
+    $.ajax({
+        type: "GET",
+        url: baseUrl + "/xyq/forum/detail?id=" + circleId,
+        success: function (msg) {
+        	console.log(msg);
+        	$("#myhead").attr("src",msg.icon);
+            $("#userName").text(msg.title);
+            $("#userRange").text('55成员');
+           /* $("#cirleName").text(msg.title)
+            $("#cirleIntroduction").text(msg.abstract || "")
+            $("#headimg").css("background-image", "url('" + msg.icon + "')")
+            $.each(msg.tags, function (i, n) {
+                $("#tag").append("<div>{0}</div>".format(n))
+            })*/
+        }
+    });
+}
+
+var initDialog = function () {
+	$("#reply-view").dialog({
+		title: "回复评论",
+		modal: false,
+		autoOpen: false,
+		width: 400,
+		height: 320,
+		open: function () {},
+		close: function () {},
+		buttons: {
+			"确定": function () {
+				creatReply();
+			},
+			"关闭": function () {
+				$(this).dialog("close");
+			}
+		}
+
+
+	})
+}
+
+
+var creatReply = function () {
+	var id = $("#replyid").val();
+	var content = $("#replyreplyCount").val();
+	$.ajax({
+		type: "POST",
+		url: baseUrl + "/xyq/subreply",
+		data: {
+			replyId: id,
+			content: content
+		},
+		success: function (msg) {
+			$("#pinglun-items").empty();
+			getreplies();
+		}
+	});
+
+
+
+
+
+}
+
+var createsubreply = function () {
+	var title = $("#replyinput").val();
+	$.ajax({
+		type: "POST",
+		url: baseUrl + "/xyq/reply",
+		data: {
+			postId: id,
+			content: title
+		},
+		success: function (msg) {
+			$("#pinglun-items").empty();
+			getreplies();
+		}
+	});
+}
 
 var getmyactivecounts = function () {
 	var ss = $("#2").html();
@@ -28,15 +136,15 @@ var getreplies = function () {
 	var data = $("#1").html();
 	$.ajax({
 		type: "GET",
-		url: baseUrl + "/xyq/replies?postId=100001&pageNo=0&pageSize=10",
+		url: baseUrl + "/xyq/replies?postId={0}&pageNo=0&pageSize=10".format(id),
 		success: function (msg) {
-			console.log(msg)
-			$.each(msg.replies,function(i,n){
+			console.log(msg);
+			$.each(msg.replies, function (i, n) {
 				var ss = '';
-				$.each(n.subReplies,function(j,s){
-					ss+="<p>回复{0}:{1}</p>".format(s.userName,s.content)
+				$.each(n.subReplies, function (j, s) {
+					ss += "<p>回复{0}:{1}</p>".format(s.userName, s.content)
 				})
-				$("#pinglun-items").append(data.format(n.icon,n.userName,1,n.content,ss,n.createTime))
+				$("#pinglun-items").append(data.format(n.icon, n.userName, n.level, n.content, ss, n.createTime, n.id))
 			})
 		}
 	});
@@ -55,6 +163,7 @@ var getParam = function () {
 			obj[item[0]] = item[1];
 		}
 		id = obj.id;
+        circleId=obj.circleId;
 		return obj;
 	} catch (e) {
 		console.warn("There has no param value!");

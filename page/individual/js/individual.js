d8b125387effce802512ee9e40001a88853fb5ad
djchen <694015726@qq.com>
2018-10-21 21:34:58 +0800

    个人中心
    个人中心代码提交

--- page/individual/js/individual.js
+++ page/individual/js/individual.js
@@ -1,5 +1,6 @@
 $(function() {
 	initClick();
+	$("#myhome").show()
 
 })
 
@@ -10,7 +11,40 @@ var initClick = function() {
 		var par = $(this).parent()[0]
 		$(par).find(".circleActive").removeClass("circleActive");
 		$(this).addClass("circleActive")
-		$(".circle-content div").hide();
+		$(".circle-content>div").hide();
 		$("#" + id).show()
 	})
+
+	$(".shouchang-menu-item").on('click', function() {
+		var id = $(this).attr("data-id");
+		var par = $(this).parent()[0]
+		$(par).find(".circleActive").removeClass("circleActive");
+		$(this).addClass("circleActive")
+		$(".shouchang-content>div").hide();
+		$("#" + id).show()
+	})
+
+	$("#menutable td").on('click', function() {
+		var par = $(this).parent()[0];
+		$(par).find(".active").removeClass("active");
+		$(this).addClass("active");
+	})
+
+	$(".wenda-content-head-item").on('click', function() {
+		$(".wenda-active").removeClass("wenda-active");
+		$(this).addClass("wenda-active");
+	})
+}
+
+
+var show = function(id) {
+	$(".content>div").hide();
+	if (id.startsWith('myhome')) {
+		$("#myhome").show();
+		$(".myhome-right-item").hide();
+		$("#right-" + id.split("-")[1]).show();
+	} else {
+		$("#" + id).show();
+	}
+
 }
